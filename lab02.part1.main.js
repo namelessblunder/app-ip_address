0e8d101acdac36eec7871bba9ee9aa00374593a0
namelessblunder <nameless.blunder19@gmail.com>
2020-08-26 10:09:38 +0000

    Lab 2 part 1 of 2

--- lab02.part1.main.js
+++ lab02.part1.main.js
@@ -11,15 +11,13 @@ const IPCIDR = require('ip-cidr');
  * @param {string} cidrStr - The IPv4 subnet expressed
  *                 in CIDR format.
  * @param {callback} callback - A callback function.
- * @return {string} (returnedIp) - An array of IPv4 address and IPv6 address.
+ * @return {string} (firstIpAddress) - An IPv4 address.
  */
 function getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
   let firstIpAddress = null;
   let callbackError = null;
-  let ipV6address = null;
-  let returnedIp = null;
 
   // Instantiate an object from the imported class and assign the instance to variable cidr.
   const cidr = new IPCIDR(cidrStr);
@@ -39,15 +37,12 @@ function getFirstIpAddress(cidrStr, callback) {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
     [firstIpAddress] = cidr.toArray(options);
-    ipV6address = getIpv4MappedIpv6Address(firstIpAddress);
   }
-
-  returnedIp = {'ipv4': firstIpAddress, 'ipv6': ipV6address};
   // Call the passed callback function.
   // Node.js convention is to pass error data as the first argument to a callback.
   // The IAP convention is to pass returned data as the first argument and error
   // data as the second argument to the callback function.
-  return callback(returnedIp, callbackError);
+  return callback(firstIpAddress, callbackError);
 }
 
 /**
@@ -122,7 +117,7 @@ function main() {
       if (error) {
         console.error(`  Error returned from GET request: ${error}`);
       }
-      console.log(`  Response returned from GET request: ${JSON.stringify(data)}`);
+      console.log(`  Response returned from GET request: ${data}`);
     });
   }
   // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
